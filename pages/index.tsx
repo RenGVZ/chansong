0d1374be0f5f2f59ba6bbad568e65c97d2e7ba81
Gavin Garza <gaving@live.com>
2022-12-27 23:17:28 +0900

    added link component and userContext

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import { SpotifyObj, User, ImageItem } from '../types'
 import {getTokenFromUrl} from '../utilities'
@@ -98,6 +99,7 @@ export default function Home() {
         >
           Login real
         </a>
+        <Link href="/artist">Go to artist page</Link>
       </div>
     </>
   )
