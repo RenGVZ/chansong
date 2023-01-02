ec16fd5fb992778898bdf39a95e58786a584ad7e
RenGVZ <gaving@live.com>
2023-01-02 22:07:04 +0900

    implemented css for Library component

--- components/Home/LibraryTab.tsx
+++ components/Home/LibraryTab.tsx
@@ -12,6 +12,15 @@ const LibraryTab = () => {
     getSavedTracks()
   }, [])
 
+  const getTrackPlaytime = (timestamp: number) => {
+    let minutes = new Date(timestamp).getMinutes()
+    if (minutes >= 60) {
+      return new Date(timestamp).toISOString().slice(11, 19)
+    } else {
+      return new Date(timestamp).toISOString().slice(14, 19)
+    }
+  }
+
   useEffect(() => {
     getSavedTracksCallback()
   }, [getSavedTracksCallback])
@@ -19,10 +28,10 @@ const LibraryTab = () => {
   console.log('savedTracks:', savedTracks);
 
   return (
-    <div className="library w-full flex flex-col shadow-cust rounded-[10px]">
-      <div className="flex w-full justify-between py-4 items-center">
+    <div className="library w-full flex flex-col shadow-cust rounded-[10px] space-y-4 py-4">
+      <div className="flex w-full justify-between items-center">
         <h1 className="text-lg uppercase pl-8">Library</h1>
-        <div className="flex flex-row w-4/12 items-baseline space-x-4">
+        <div className="flex flex-row md:w-4/12 items-baseline space-x-4">
           <div className="flex w-8/12 items-center shadow-library-search rounded-full p-1.5">
             <FontAwesomeIcon icon={faMagnifyingGlass} className="text-xs text-mid px-2" />
             <p className="text-dark text-xs">Search</p>
@@ -30,18 +39,26 @@ const LibraryTab = () => {
           <p className="text-dark">See More</p>
         </div>
       </div>
-      <div className="tracks">
+      <div className="tracks flex flex-col space-y-3">
         {savedTracks && savedTracks?.items?.slice(0, 5).map((item: SavedTracks, i) => (
-          <div key={i} className="flex items-center">
-            <Image src={item.track.album?.images[0].url} width={40} height={40} alt=""></Image>
-            <div className="flex flex-col">
-              <p className="text-lg">{item.track.album.artists && item.track.album.artists[0].name}</p>
-              <p className="text-sm">{item.track.name}</p>
+          <div key={i} className="flex items-center justify-between px-6">
+            <div className="flex items-center space-x-3">
+              <Image className="rounded-[10px] object-cover" src={item.track.album?.images[0].url} width={42} height={42} alt=""></Image>
+              <div className="flex flex-col">
+                <p className="text-lg leading-none">{item.track.album.artists && item.track.album.artists[0].name}</p>
+                <p className="text-sm leading-none">{item.track.name}</p>
+              </div>
             </div>
-            <FontAwesomeIcon icon={faHeart} className="text-xs" />
-            <FontAwesomeIcon icon={faCircleMinus} className="text-xs" />
-            <p>{new Date(item?.track?.duration_ms).toISOString().slice(14, 19)}</p>
-            <FontAwesomeIcon icon={faPlay} className="text-xs" />
+            <div className="flex items-center md:w-4/12">
+              <div className="w-1/2 flex items-center justify-start space-x-6 pl-4">
+                <FontAwesomeIcon icon={faHeart} className="text-xs" width={12} />
+                <FontAwesomeIcon icon={faCircleMinus} className="text-xs" width={12} />
+              </div>
+              <div className="w-1/2 flex items-center justify-end space-x-6">
+                <p>{getTrackPlaytime(item.track.duration_ms)}</p>
+                <FontAwesomeIcon icon={faPlay} className="text-xs" width={12} />
+              </div>
+            </div>
           </div>
         ))}
       </div>
