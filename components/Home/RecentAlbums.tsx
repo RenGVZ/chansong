e6344b77b22028de609ea0c35942ba9caf7f6e60
RenGVZ <gaving@live.com>
2023-01-04 23:43:10 +0900

    added RecentAlbums component

---
+++ components/Home/RecentAlbums.tsx
@@ -0,0 +1,11 @@
+import SectionContainer from "../SectionContainer"
+
+const RecentAlbums = () => {
+  return (
+    <SectionContainer>
+
+    </SectionContainer>
+  )
+}
+
+export default RecentAlbums
