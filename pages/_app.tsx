a9c7e49532a5185e6b7f23ba738850faecf13216
RenGVZ <gaving@live.com>
2023-04-08 20:05:36 +0900

    debudding fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Just_Me_Again_Down_Here } from '@next/font/google'
-import { UserContextProvider } from '../context/SpotifyUserContext'
-import MainContainer from '../components/MainContainer'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Just_Me_Again_Down_Here } from '@next/font/google';
+import { UserContextProvider } from '../context/SpotifyUserContext';
+import MainContainer from '../components/MainContainer';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 const customFont = Just_Me_Again_Down_Here({
   subsets: ['latin'],
-  weight: ['400']
-})
+  weight: ['400'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={customFont.className}>
-      <UserContextProvider>
-        <MainContainer>
-          <Component {...pageProps} />
-        </MainContainer>
-      </UserContextProvider>
+      {/* <UserContextProvider> */}
+      {/* <MainContainer> */}
+      {/* <Component {...pageProps} /> */}
+      {/* </MainContainer> */}
+      {/* </UserContextProvider> */}
     </main>
-  )
+  );
 }
